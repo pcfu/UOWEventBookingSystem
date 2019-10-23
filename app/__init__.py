258b002c1036388fbb475462f2bbfcd4ba04c81d
pcfu <fupeichong@gmail.com>
2019-10-23 15:48:03 +0800

    Added naming convention to to db to faciliate dropping constraints

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from app.config import Config
+from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_admin import Admin
 from flask_admin.menu import MenuLink
 
+convention = {
+	"ix": 'ix_%(column_0_label)s',
+	"uq": "uq_%(table_name)s_%(column_0_name)s",
+	"ck": "ck_%(table_name)s_%(column_0_name)s",
+	"fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+	"pk": "pk_%(table_name)s"
+}
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app) # DB instance
+db = SQLAlchemy(app, metadata=MetaData(naming_convention=convention)) # DB instance
 migrate = Migrate(app, db, render_as_batch=True) # Flask database migration manager
 login_manager = LoginManager(app) # Flask login_manager
 
