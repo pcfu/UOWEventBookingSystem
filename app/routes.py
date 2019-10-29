1424b5878d93e71d76f76a9e0fd647f832feb6eb
pcfu <fupeichong@gmail.com>
2019-10-30 01:52:29 +0800

    temp

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,15 @@
 from app import app
+from app.models.users import User
 from flask import render_template, redirect, url_for
+from flask_login import current_user, login_user, logout_user
 
 
 @app.login_manager.user_loader
 def load_user(entry):
-	return True
+	if entry[0] == 'User':
+		return User.query.get(int(entry[1]))
+	else:
+		return False
 
 
 @app.route('/')
@@ -16,7 +21,19 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def user_login():
-	return redirect(url_for('index'))
+	# if already logged in redirect to homepage
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+
+	form = MemberLoginForm()
+	if form.validate_on_submit():
+		# if login is successful, return user to 'index'
+		user = User.query.filter_by(username=form.username.data).first()
+		login_user(user, remember=form.remember_me.data)
+		return redirect(url_for('index'))
+
+	# render login page
+	return render_template('login.html', title='EBS: Sign In', form=form)
 
 
 @app.route('/staff_login', methods=['GET', 'POST'])
@@ -27,4 +44,10 @@ def staff_login():
 @app.route('/logout')
 def logout():
 	#logout_user()
-	return redirect(url_for('main'))
+	return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	# if already logged in redirect to homepage
+	return redirect(url_for('index'))
