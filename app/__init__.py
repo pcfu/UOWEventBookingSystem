8fb1b95b62e67a8f8d2dc4dcdb537a87a7203cd5
pcfu <fupeichong@gmail.com>
2019-10-31 22:20:54 +0800

    minor bugfix: Typo in Admin page navbar

--- app/__init__.py
+++ app/__init__.py
@@ -35,7 +35,7 @@ from app.models import users, events, booking
 from app.views import views
 
 
-admin = Admin(app, name='UOW Event System', template_mode='bootstrap3',
+admin = Admin(app, name='UOW EBS', template_mode='bootstrap3',
 			  index_view=views.GlobalIndexView())
 admin.add_view(views.StaffVenueView(events.Venue, db.session))
 admin.add_view(views.StaffEventTypeView(events.EventType, db.session))
