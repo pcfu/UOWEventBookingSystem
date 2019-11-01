0402cdf91fc70aad6ccb1d7dc3838d0bd1dca037
pcfu <fupeichong@gmail.com>
2019-11-02 04:43:30 +0800

    db: LogoutHistory. fix id naming errro

--- app/models/logs.py
+++ app/models/logs.py
@@ -54,7 +54,7 @@ class LoginHistory(db.Model):
 
 
 class LogoutHistory(db.Model):
-	in_id = db.Column(db.Integer, primary_key=True)
+	out_id = db.Column(db.Integer, primary_key=True)
 	timestamp = db.Column(db.DateTime, nullable=False)
 	user_id = db.Column(db.Integer, ForeignKey('user.user_id'))
 	admin_id = db.Column(db.Integer, ForeignKey('admin.admin_id'))
@@ -104,5 +104,14 @@ def add_login_record():
 		log = LoginHistory(timestamp=datetime.now(), admin_id=current_user.admin_id)
 	else:
 		log = LoginHistory(timestamp=datetime.now(), user_id=current_user.user_id)
+	db.session.add(log)
+	db.session.commit()
+
+
+def add_logout_record():
+	if is_admin_user():
+		log = LogoutHistory(timestamp=datetime.now(), admin_id=current_user.admin_id)
+	else:
+		log = LogoutHistory(timestamp=datetime.now(), user_id=current_user.user_id)
 	db.session.add(log)
 	db.session.commit()
