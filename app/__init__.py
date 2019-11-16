ec537007abbc0ccbdf89f0ee0b472acebcbdacf7
pcfu <fupeichong@gmail.com>
2019-11-17 07:14:43 +0800

    models: add UserGroup and User

--- app/__init__.py
+++ app/__init__.py
@@ -48,7 +48,7 @@ if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
 
 
 from app import routes
-#from app.models import users, events, booking, logs, payments
+from app.models import users #, events, booking, logs, payments
 #from app.views import views
 
 
--- app/models/users.py
+++ app/models/users.py
@@ -4,22 +4,32 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import ForeignKey
 
 
+class UserGroup(db.Model):
+	__tablename__ = 'usergroup'
+	group_id = db.Column(db.Integer, primary_key=True)
+	group_name = db.Column(db.String, index=True, unique=True, nullable=False)
+
+	users = db.relationship('User', back_populates='group')
+
+
 class User(UserMixin, db.Model):
+	__tablename__ = 'user'
 	user_id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(30), index=True, unique=True, nullable=False)
 	email = db.Column(db.String(30), index=True, unique=True, nullable=False)
 	password_hash = db.Column(db.String(255), nullable=False)
-	is_staff = db.Column(db.Boolean, nullable=False)
+	group_id = db.Column(db.Integer, ForeignKey('usergroup.group_id'))
 
-	logins = db.relationship('LoginHistory', back_populates='user')
-	logouts = db.relationship('LogoutHistory', back_populates='user')
-	bookings = db.relationship('Booking', back_populates='user')
+	group = db.relationship('UserGroup', back_populates='users')
+	#logins = db.relationship('LoginHistory', back_populates='user')
+	#logouts = db.relationship('LogoutHistory', back_populates='user')
+	#bookings = db.relationship('Booking', back_populates='user')
 
 	def __repr__(self):
 		return '[ UID:{:0>4} ] {}'.format(self.user_id, self.username)
 
-	def get_id(self):
-		return ('User', self.user_id)
+	#def get_id(self):
+	#	return ('User', self.user_id)
 
 	def set_password(self, password):
 		self.password_hash = generate_password_hash(password)
@@ -28,6 +38,7 @@ class User(UserMixin, db.Model):
 		return check_password_hash(self.password_hash, password)
 
 
+'''
 class Admin(UserMixin, db.Model):
 	admin_id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(30), index=True, unique=True, nullable=False)
@@ -48,3 +59,4 @@ class Admin(UserMixin, db.Model):
 
 	def check_password(self, password):
 		return check_password_hash(self.password_hash, password)
+'''
