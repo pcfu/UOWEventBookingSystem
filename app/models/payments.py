f5155890580a69f82a50047f7f2c0c006c93b3bf
pcfu <fupeichong@gmail.com>
2019-11-18 06:41:39 +0800

    models: add Promotion

--- app/models/payments.py
+++ app/models/payments.py
@@ -10,10 +10,10 @@ class Payment(db.Model):
 	amount = db.Column(db.Float, nullable=False)
 	card_number = db.Column(db.Integer, nullable=False)
 	booking_id = db.Column(db.Integer, ForeignKey('booking.booking_id'), nullable=False)
-	#promotion_id = db.Column(db.Integer, ForeignKey('promotion.promotion_id'))
+	promotion_id = db.Column(db.Integer, ForeignKey('promotion.promotion_id'))
 
 	booking = db.relationship('Booking', back_populates='payments')
-	#promotion = db.relationship('Promotion', back_populates='payments')
+	promotion = db.relationship('Promotion', back_populates='payments')
 	#refunds = db.relationship('Refund', back_populates='payment')
 
 	def __repr__(self):
@@ -38,7 +38,7 @@ class Payment(db.Model):
 		return self.quantity == self.total_refund_qty
 	'''
 
-'''
+
 class Promotion(db.Model):
 	promotion_id = db.Column(db.Integer, primary_key=True)
 	promo_percentage = db.Column(db.Integer, nullable=False)
@@ -47,11 +47,12 @@ class Promotion(db.Model):
 	promo_code = db.Column(db.String, nullable=False)
 
 	payments = db.relationship('Payment', back_populates='promotion')
-	event_pairings = db.relationship('EventPromotion', cascade='all, delete', back_populates='promotion')
+	#event_pairings = db.relationship('EventPromotion', cascade='all, delete', back_populates='promotion')
 
 	def __repr__(self):
 		return '[ PROID:{:0>4} ] {}'.format(self.promotion_id, self.promo_code)
 
+	'''
 	@hybrid_property
 	def has_active_event_promo(self):
 		active_pairs = 0
@@ -80,8 +81,10 @@ class Promotion(db.Model):
 							else_=False)
 				]).where(Payment.promotion_id == cls.promotion_id)\
 				 .correlate(cls).as_scalar()
+	'''
 
 
+'''
 class EventPromotion(db.Model):
 	event_id = db.Column(db.Integer, ForeignKey('event.event_id'), primary_key=True)
 	promotion_id = db.Column(db.Integer, ForeignKey('promotion.promotion_id'), primary_key=True)
